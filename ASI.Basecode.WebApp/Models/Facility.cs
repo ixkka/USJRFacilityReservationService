015b9b172244298041e8b0eb85bd429882e48b58
gkg422001 <giankristoffgerodias@gmail.com>
2024-10-26 09:22:05 -0700

    Linked the screens for facilities

---
+++ ASI.Basecode.WebApp/Models/Facility.cs
@@ -0,0 +1,11 @@
+﻿namespace ASI.Basecode.WebApp.Models
+{
+    public class FacilityViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public decimal PricePerDay { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
